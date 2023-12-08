7225d693b7e6d1adcbefd51564a3d766165b2e77
Montassarkhe <111145504+Montassarkhe@users.noreply.github.com>
2023-12-08 16:17:38 +0100

    api.js

--- api.js
+++ api.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// Create an Express application
+// create an Express application
 const app = express();
 const PORT = 3000;
 
 // parse JSON data in incoming requests
 app.use(bodyParser.json());
 
-// Define an API endpoint to handle incoming sensor data
+// define an API endpoint to handle incoming sensor data
 app.post('/api/sensors', (req, res) => {
-    // Extract the sensor data from the request body
+    // extract the sensor data from the request body
     const sensorData = req.body;
 
     const { Pool } = require('pg');
 
-    // Create a cnx pool with our db
+    // create a cnx pool with our db
     const pool = new Pool({
        user: 'postgres',
        host: 'localhost',
@@ -24,7 +24,7 @@ app.post('/api/sensors', (req, res) => {
        port: 5432,
 });
 
-// Store the data to the db
+// store the data to the db
 pool.query(
     'INSERT INTO sensors_table (serial, swVersion, temperature, date, gps) VALUES ($1, $2, $3, $4, $5)',
     [
@@ -41,16 +41,17 @@ pool.query(
         console.log('Sensor data stored in the database.');
     }
 );
-    // See thz stored data in server logs
+    // see thz stored data in server logs
     console.log('Received Sensor Data:', sensorData);
 
     pool.end(); 
 
-    // Send a response back to the client
+    // send a response back to the client
     res.send('Sensor data received and stored.');
 });
 
-// Start the server and lissten on port 3000
+// start the server and lissten on port 3000
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
